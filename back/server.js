acfbf2cf6549db3cec0660c05aedb755f1b9abdd
Pascal <fedodev55@gmail.com>
2019-12-04 17:56:21 +0100

    configure files server and conf

--- back/server.js
+++ back/server.js
@@ -1,12 +1,23 @@
-import conf from './conf'
+const connection = require('./conf')
 const express = require("express")
 const app = express()
-app.listen(8000,()=>console.log('Hello Folks'))
+const port = 8000;
 
-app.get('/', function (req, res) {
-    res.send('hello world')
-});
+// CONNECTION PORT ///////////////////////////////////////////////////
+app.listen(port, (err) => {
+    if (err) {
+      throw new Error('Error listening port ...');
+    }
+    // console.log(`Server is listening on ${port}`);
+  });
 
-app.get('/offer', function (req, res) {
-    res.send('welcome in offer')
-});
+//NCREATION ROUTES /////////////////////////////////////////////////
+app.get('/offer', (req, res) => {
+    connection.query('SELECT * from offer', (err, results) => {
+      if (err) {
+        res.status(500).send('Error server 500');
+      } else {
+        res.json(results);
+      }
+    });
+  });
