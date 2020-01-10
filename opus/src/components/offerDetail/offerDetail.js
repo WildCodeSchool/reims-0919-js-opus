3e05b6a7f1e1660242f9b38ddf7626c9c53df4b7
Christopher Poncelet <christopher.poncelet51@gmail.com>
2020-01-10 11:32:36 +0100

    create carousel css offerDetail

--- opus/src/components/offerDetail/offerDetail.js
+++ opus/src/components/offerDetail/offerDetail.js
@@ -1,53 +1,92 @@
 import React from 'react';
 import './offerDetail.css';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 class offerDetail extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
   render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+
     return (
       <div className="divDetail">
         <img
           className="officeImage"
-          src="https://via.placeholder.com/50"
+          src={this.props.location.state.offerImage}
           alt="office"
         />
 
         <div className="detail">
-          <h1>Salle de meeting</h1>
-          <div className="info">
-            <p>Reims</p>
-            <p>8p 50€</p>
+          <h1>{this.props.location.state.titleOffer}</h1>
+          <div className="infoDetail">
+            <p>{this.props.location.state.addressCity}</p>
+            <p>
+              {this.props.location.state.capacityOffice} places{' '}
+              {this.props.location.state.priceOffer}€
+            </p>
           </div>
         </div>
 
         <hr />
-
-        <p className="textDetail">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor
-          sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum
-        </p>
-
-        <div>
+        <div className="divTextDetail">
+          <p className="textDetail">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </p>
+        </div>
+        <div className="sliderPhotos">
           <h2>Photos</h2>
-          <img className="officeImage" src="https://via.placeholder.com/50" />
+
+          <Slider {...settings}>
+            <div>
+              <img
+                className="officeImage"
+                src="https://cdn.pixabay.com/photo/2017/03/28/12/11/chairs-2181960_960_720.jpg"
+              />
+            </div>
+            <div>
+              <img
+                className="officeImage"
+                src="https://cdn.pixabay.com/photo/2015/05/15/14/22/conference-room-768441_960_720.jpg"
+              />
+            </div>
+            <div>
+              <img
+                className="officeImage"
+                src="https://cdn.pixabay.com/photo/2017/03/28/12/06/chairs-2181916_960_720.jpg"
+              />
+            </div>
+          </Slider>
         </div>
 
-        <div>
+        <div className="equipements">
           <h2>Equipements</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam.
+          </p>
         </div>
 
-        <input className="signIn" type="button" value="Valider" />
+        <input className="validateOffer" type="button" value="Valider" />
       </div>
     );
   }
