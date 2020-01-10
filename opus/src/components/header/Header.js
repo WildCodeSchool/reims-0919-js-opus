8ea8658da3839d940362508072c3cd2347496fb8
Pascal <fedodev55@gmail.com>
2020-01-10 12:08:58 +0100

    prepare button filter

--- opus/src/components/header/Header.js
+++ opus/src/components/header/Header.js
@@ -1,35 +1,52 @@
-import React from 'react';
+//import React from 'react';
 import './Header.css';
 import filter from './fil.png';
 
-const Header = ({ searchCity, handleChange, handleSubmit }) => {
-  return (
-    <div className="menu-header">
-      <nav className="header">
-        <input
-          className="searchOffer"
-          type="search"
-          // id="searchOffer"
-          placeholder="Recherche"
-          value={searchCity}
-          onChange={handleChange}
-        />
-        <input
-          className="arrow-submit"
-          type="image"
-          // id="arrow-submit"
-          src="https://i.ibb.co/6b7rbrw/Fl-che-droite-rond-rose-50x50px.png"
-          alt="arrow"
-          onClick={handleSubmit}
-        />
+import React, { Component } from 'react';
 
-        <button className="btnFilter">
-          <img className="iconeFilter" src={filter} alt="icone" />
-          Filtre
-        </button>
-      </nav>
-    </div>
-  );
-};
+export default class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+    this.showFilters = this.showFilters.bind(this);
+  }
+  showFilters() {
+    this.setState({ show: true });
+  }
 
-export default Header;
+  render() {
+    return (
+      // <div className="aaa">
+      <div className="menu-header">
+        <nav className="header">
+          <input
+            className="searchOffer"
+            type="search"
+            placeholder="Recherche"
+            value={this.props.searchCity}
+            onChange={this.props.handleChange}
+          />
+          <input
+            className="arrow-submit"
+            type="image"
+            src="https://i.ibb.co/6b7rbrw/Fl-che-droite-rond-rose-50x50px.png"
+            alt="arrow"
+            onClick={this.props.handleSubmit}
+          />
+
+          <button className="btnFilter" onClick={this.showFilters}>
+            <img className="iconeFilter" src={filter} alt="icone" />
+            Filtre
+          </button>
+        </nav>
+
+        {/* <button className="btnFilter">
+            <img className="iconeFilter" src={filter} alt="icone" />
+            Filtre
+          </button> */}
+      </div>
+    );
+  }
+}
