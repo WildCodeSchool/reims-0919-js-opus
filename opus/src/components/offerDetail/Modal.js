edbe17416205b6f4bf3cd6dd7d3086d3e925281a
Pascal <fedodev55@gmail.com>
2020-01-22 16:15:54 +0100

    create booking calendar

--- opus/src/components/offerDetail/Modal.js
+++ opus/src/components/offerDetail/Modal.js
@@ -1,30 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './OfferDetail.css';
+import MomentInput from 'react-moment-input';
+import moment from 'moment';
 
-const Modal = props => {
-  // const Modal = ({ visible, cache }) => {
-  return (
-    <div
-      className="modal"
-      style={{
-        transform: props.visibleModal ? 'translateY(0vh' : 'translateY(-100vh)',
-        opacity: props.visibleModal ? '.9' : '0'
-      }}
-    >
-      <button className="btnCloseModale" onClick={props.closeModal}>
-        X
-      </button>
-      <h3>Plage de réservation</h3>
-      <p>
-        Holisticly build holistic content vis-a-vis competitive growth
-        strategies. Appropriately visualize highly efficient paradigms after
-        long-term high-impact opportunities. Conveniently predominate seamless
-        paradigms vis-a-vis cross-platform benefits. Objectively productivate
-        unique leadership skills via cross-media infrastructures. Appropriately
-        communicate stand-alone testing procedures and.
-      </p>
-    </div>
-  );
-};
+export default class Modal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dateDeb: null,
+      dateFin: null
+    };
+  }
 
-export default Modal;
+  render() {
+    return (
+      <div
+        className="modal"
+        style={{
+          transform: this.props.openModal
+            ? 'translateY(0vh'
+            : 'translateY(-100vh)',
+          opacity: this.props.openModal ? '1' : '0'
+        }}
+      >
+        <button className="btnCloseModale" onClick={this.props.closeModal}>
+          X
+        </button>
+        <div>
+          {/* //////////////////////////// CLALENDAR ////////////////////////////////////////// */}
+          <h2>Date de début</h2>
+
+          <MomentInput
+            translations={{
+              DATE: 'Date',
+              TIME: 'Heure',
+              DAYS_MON: 'Lun',
+              DAYS_TUE: 'Mar',
+              DAYS_WED: 'Mer',
+              DAYS_THU: 'Jeu',
+              DAYS_FRI: 'Ven',
+              DAYS_SAT: 'Sam',
+              DAYS_SUN: 'Dim',
+              MONTHS_JANUARY: 'Janvier',
+              MONTHS_FEBRUARY: 'Fevrier',
+              MONTHS_MARCH: 'Mars',
+              MONTHS_APRIL: 'Avril',
+              MONTHS_MAY: 'Mai',
+              MONTHS_JUNE: 'Juin',
+              MONTHS_JULY: 'Juillet',
+              MONTHS_AUGUST: 'Aout',
+              MONTHS_SEPTEMBER: 'Septembre',
+              MONTHS_OCTOBER: 'Octobre',
+              MONTHS_NOVEMBER: 'Novembre',
+              MONTHS_DECEMBER: 'Decembre'
+            }}
+            format="DD-MM-YYYY HH:mm"
+            options={true}
+            readOnly={false}
+            icon={true}
+            enableInputClick={true}
+            onClose={date =>
+              this.setState({ dateDeb: moment(date._d).format('L HH:mm:ss') })
+            }
+          />
+
+          <h2>Date de fin</h2>
+
+          <MomentInput
+            translations={{
+              DATE: 'Date',
+              TIME: 'Heure',
+              DAYS_MON: 'Lun',
+              DAYS_TUE: 'Mar',
+              MONTHS_JANUARY: 'Janvier',
+              MONTHS_OCTOBER: 'Octobre'
+            }}
+            format="DD-MM-YYYY HH:mm"
+            options={true}
+            readOnly={false}
+            icon={true}
+            enableInputClick={true}
+            onClose={date =>
+              this.setState({ dateFin: moment(date._d).format('L HH:mm:ss') })
+            }
+          />
+          <button className="btnValidDate">Enregistrer</button>
+        </div>
+      </div>
+    );
+  }
+}
--- opus/src/components/offerDetail/OfferDetail.js
+++ opus/src/components/offerDetail/OfferDetail.js
@@ -6,6 +6,7 @@ import 'slick-carousel/slick/slick-theme.css';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import Modal from './Modal';
 
 const mapStateToProps = state => ({
   token: state.token
@@ -16,7 +17,8 @@ class OfferDetail extends React.Component {
     super(props);
 
     this.state = {
-      reservation_date: ''
+      reservation_date: '',
+      visibleModal: false
     };
   }
 
@@ -50,6 +52,13 @@ class OfferDetail extends React.Component {
     window.scrollTo(0, 0);
   }
 
+  openModal = () => {
+    this.setState({ visibleModal: true });
+  };
+  closeModal = () => {
+    this.setState({ visibleModal: false });
+  };
+
   render() {
     const settings = {
       dots: true,
@@ -60,82 +69,85 @@ class OfferDetail extends React.Component {
     };
 
     return this.props.token ? (
-      <div className="divDetail">
-        <img
-          className="officeImage"
-          src={this.props.location.state.offerImage}
-          alt="office"
-        />
+      <>
+        <div className="divDetail">
+          <img
+            className="officeImage"
+            src={this.props.location.state.offerImage}
+            alt="office"
+          />
 
-        <div className="detail">
-          <h1>{this.props.location.state.titleOffer}</h1>
-          <div className="infoDetail">
-            <p>{this.props.location.state.addressCity}</p>
-            <p>
-              {this.props.location.state.capacityOffice} places{' '}
-              {this.props.location.state.priceOffer}€
+          <div className="detail">
+            <h1>{this.props.location.state.titleOffer}</h1>
+            <div className="infoDetail">
+              <p>{this.props.location.state.addressCity}</p>
+              <p>
+                {this.props.location.state.capacityOffice} places{' '}
+                {this.props.location.state.priceOffer}€
+              </p>
+            </div>
+          </div>
+
+          <hr className="hrOfferDetail" />
+          <div className="divTextDetail">
+            <p className="textDetail">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur.
             </p>
           </div>
+          <div className="sliderPhotos">
+            <h2>Photos</h2>
+
+            <Slider {...settings}>
+              <div>
+                <img
+                  className="officeImage"
+                  src="https://cdn.pixabay.com/photo/2017/03/28/12/11/chairs-2181960_960_720.jpg"
+                  alt="office1"
+                />
+              </div>
+              <div>
+                <img
+                  className="officeImage"
+                  src="https://cdn.pixabay.com/photo/2015/05/15/14/22/conference-room-768441_960_720.jpg"
+                  alt="office2"
+                />
+              </div>
+              <div>
+                <img
+                  className="officeImage"
+                  src="https://cdn.pixabay.com/photo/2017/03/28/12/06/chairs-2181916_960_720.jpg"
+                  alt="office3"
+                />
+              </div>
+            </Slider>
+          </div>
+
+          <div className="equipements">
+            <h2>Equipements</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam.
+            </p>
+          </div>
+
+          <input
+            className="validateOffer"
+            type="button"
+            value="Réserver"
+            onClick={this.openModal}
+          />
         </div>
-
-        <hr className="hrOfferDetail" />
-        <div className="divTextDetail">
-          <p className="textDetail">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.
-          </p>
-        </div>
-        <div className="sliderPhotos">
-          <h2>Photos</h2>
-
-          <Slider {...settings}>
-            <div>
-              <img
-                className="officeImage"
-                src="https://cdn.pixabay.com/photo/2017/03/28/12/11/chairs-2181960_960_720.jpg"
-                alt="office1"
-              />
-            </div>
-            <div>
-              <img
-                className="officeImage"
-                src="https://cdn.pixabay.com/photo/2015/05/15/14/22/conference-room-768441_960_720.jpg"
-                alt="office2"
-              />
-            </div>
-            <div>
-              <img
-                className="officeImage"
-                src="https://cdn.pixabay.com/photo/2017/03/28/12/06/chairs-2181916_960_720.jpg"
-                alt="office3"
-              />
-            </div>
-          </Slider>
-        </div>
-
-        <div className="equipements">
-          <h2>Equipements</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam.
-          </p>
-        </div>
-
-        <input className="validateOffer" type="button" value="Réserver" />
-
-        <input
-          className="dateInput"
-          type="text"
-          placeholder="Date"
-          value={this.state.reservation_date}
-          onChange={this.handleChange}
+        <Modal
+          openModal={this.state.visibleModal}
+          closeModal={this.closeModal}
         />
-      </div>
+      </>
     ) : (
       <Redirect to="/" />
     );
